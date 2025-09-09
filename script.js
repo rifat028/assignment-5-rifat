fbb3fb5b33210cd105ad37a266b8e6f5b720129a
rifat028 <istiakrifat001@gmail.com>
2025-09-09 15:21:30 +0600

    call function implemented

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ function setValue(id,currentValue,change){
     document.getElementById(id).innerText=currentValue+change
 }
 
+function addHistory(serviceName,serviceNo){
+    const historyContainer=document.getElementById('history-container')
+    const timeNow = new Date().toLocaleTimeString()
+    const historyChield=`
+        <div class=" p-4 rounded-lg bg-[#FAFAFA] mt-4 flex flex-col md:flex-row md:items-center md:justify-between">
+            <div>
+                <h2 class="text-xs md:text-[18px] font-bold">${serviceName}</h2>
+                <h3 class="text-xs md:text-[18px] text-gray-700">${serviceNo}</h3>
+            </div>
+            <div class="text-xs md:text-[18px] mt-2 md:mt-0">${timeNow}</div>
+        </div>
+    `
+    historyContainer.innerHTML=historyChield+historyContainer.innerHTML
+}
+
 
 const addHearts = document.getElementsByClassName('add-heart')
 for(const addHeart of addHearts){
@@ -20,9 +35,23 @@ for(const call of calls){
     call.addEventListener('click', function (event){
         const serviceName=call.parentElement.parentElement.getElementsByTagName('div')[3].getElementsByTagName('p')[0].innerText
         const serviceNo=call.parentElement.parentElement.getElementsByTagName('div')[3].getElementsByTagName('h1')[0].innerText
-        const callAlert='📞you are calling '+serviceName+' at '+serviceNo
-        alert(callAlert)
+        const coinCount=getValue("coin-count")
+        if(coinCount>=20){
+            const callAlert='📞you are calling '+serviceName+' at '+serviceNo
+            alert(callAlert)
+            const deductValu=-20  
+            setValue('coin-count',coinCount,deductValu)
+            addHistory(serviceName,serviceNo)
+        }
+        else{
+            alert('You do not have sufficient coin')
+            return
+        }
     })
 }
+
+document.getElementById('clear-history').addEventListener('click', function (){
+    document.getElementById('history-container').innerHTML=''
+})
 
 
